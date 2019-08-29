116c3e86cd53b3038690946ab8ec921541d164bf
Thiago Coutinho <thcoutinho1997@gmail.com>
2019-08-29 20:57:12 -0300

    Bytebank

---
+++ bytebank/src/CriaConta.java
@@ -0,0 +1,17 @@
+
+public class CriaConta {
+
+	public static void main(String[] args) {
+		Conta Conta1 = new Conta();
+		Conta1.saldo	=	200.50;
+		
+		while (Conta1.saldo	< 2.000)	{
+			Conta1.saldo++;
+			
+			System.out.println(Conta1.saldo);
+			System.out.println("THIAGO");
+		}
+		
+		
+	}
+}
